e626d3efabe01790a7415ee49be543682c7113a5
Nicholas Hobbs <nick@nhobb.com>
2024-12-01 17:06:18 +1100

    Solve part 1

--- day01/main.lua
+++ day01/main.lua
@@ -1,30 +1,29 @@
-input = io.open("realdata.txt", "r")
+input = io.open(arg[1], "r")
 
 function parser (inputLine)
   local x,y = inputLine:match("(%d+)%s+(%d+)")
   return tonumber(x), tonumber(y)
 end
 
-minX, minX2 = nil, nil
-minY, minY2 = nil, nil
+X = {}
+Y = {}
 
 for line in input:lines() do
-  x,y = parser(line)
-  if (not minX or x <= minX) then
-    minX2 = minX
-    minX = x
-  elseif (not minX2 or x < minX2) then
-    minX2 = x
-  end
-
-  if (not minY or y <= minY) then
-    minY2 = minY
-    minY = y
-  elseif (not minY2 or y < minY2) then
-    minY2 = y
-  end
+  local x,y = parser(line)
+  X[#X + 1] = x
+  Y[#Y + 1] = y
 end
 
-print(minX, minX2)
-print(minY, minY2)
+table.sort(X)
+table.sort(Y)
+
+distance = 0
+
+for i in pairs(X) do
+  local x = X[i]
+  local y = Y[i]
+  distance = distance + math.abs(x-y)
+end
+
+print(distance)
 
