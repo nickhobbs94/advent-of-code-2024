c9c6bcf7f638e5ebccfbcd1d18508a08f228b118
Nicholas Hobbs <nick@nhobb.com>
2024-12-02 19:26:39 +1100

    Commit WIP

--- day02/main.lua
+++ day02/main.lua
@@ -20,28 +20,54 @@ end
 safecount = 0
 unsafecount = 0
 
+function valid(dir, prev, curr)
+  if (dir == "up" and prev > curr) then return false end
+  if (dir == "down" and prev < curr) then return false end
+  local delta = math.abs(prev - curr)
+  if (delta == 0 or delta > 3) then return false end
+  return true
+end
+
 for line in input:lines() do
   local prev = nil
   local prev2 = nil
   local dir = nil
-  local dir2 = nil
   local safe = true
-  local safe2 = true
+  local skipped = false
 
   for s in line:gmatch("%d+") do
     local n = tonumber(s)
-    if (prev ~= nil) then 
+
+    if (prev2) then
+      local dir2 = setDir(dir, prev2, n)
+      dir = setDir(dir, prev2, prev)
       dir = setDir(dir, prev, n)
-      if (prev < n and dir == "down") then safe = false end
-      if (prev > n and dir == "up") then safe = false end
-      
-      local delta = math.abs(n - prev)
-      if (delta == 0 or delta > 3) then safe = false end
+
+      -- prev2 -> prev
+      local goodPrev = valid(dir, prev2, prev)
+      -- prev -> n
+      local good = valid(dir, prev, n)
+      -- or
+      -- prev2 -> n
+      local goodSkip = valid(dir2, prev2, n)
+
+      if (not good) then
+        -- - 1 0
+        if (not skipped and goodPrev) then
+          skipped = true
+          prev = 
+        else
+          safe = false
+        end
+      end
+
     end
 
     prev2 = prev
     prev = n
   end
+
+  if (not prev2) then print("NOT ENOUGH") end
 
   --print(safe and "safe" or "unsafe")
   if (safe) then
