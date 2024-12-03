77d82e4d6071978b8d2079b359d2011c4111c7af
Nicholas Hobbs <nick@nhobb.com>
2024-12-03 17:23:29 +1100

    Part 1 day 3

--- day03/main.lua
+++ day03/main.lua
@@ -1,15 +1,20 @@
 #!/usr/bin/env lua
 input = io.open(arg[1], "r")
 
-mul = 7
+mul = function (a,b) return tonumber(a)*tonumber(b) end
 
-instructions = {"mul"= mul,}
+instructions = {}
+instructions.mul = mul
 
-print(instructions["mul"])
-
+total = 0
 for line in input:lines() do
-  for s in line:gmatch("%d+") do
-    --print(s)
+  for s in line:gmatch("mul%(%d+,%d+%)") do
+    local ins, a, b = s:match("(mul)%((%d+),(%d+)%)")
+    print(ins, a, b)
+    local eval = instructions[ins](a,b)
+    print(eval)
+    total = total + eval
   end
 end
+print(total)
 
