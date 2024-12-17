d6920fa33bf3ae778e3de4a2d6cc73cb693fc54c
Nicholas Hobbs <nick@nhobb.com>
2024-12-17 21:05:36 +1100

    Commit in-progress day 12

--- day12/main.lua
+++ day12/main.lua
@@ -37,7 +37,8 @@ end
 
 local regionmap = {}
 function defaultregion(plantletter)
-  return {perim=0, area=0, plant=plantletter}
+  print("New region", plantletter)
+  return {perim=0, area=0, plant=plantletter, sides=-4}
 end
 
 function lookup(x,y)
@@ -54,7 +55,7 @@ end
 local allregions = {}
 
 function setneighbours(x,y,region)
-  print("setneighbours", x, y, region.plant)
+  --print("setneighbours", region.plant, x, y)
   for _,n in ipairs(neighbours(x,y)) do
     if isplant(n[1], n[2], region.plant) then
       if lookup(n[1], n[2]) == nil then
@@ -93,23 +94,58 @@ for y=1,height do
     local region = findregion(x,y,plant)
     region.area = region.area + 1
 
-    if plant == 'C' then
-      print(region.area)
-    end
+    local neebs = {}
+    neebs["-1,0"] = false -- left
+    neebs["1,0"] = false -- right
+    neebs["0,-1"] = false -- up
+    neebs["0,1"] = false -- down
 
+    local count = 0
     for _,neighbour in ipairs(neighbours(x,y)) do
       if isplant(neighbour[1], neighbour[2], plant) then
+        count = count + 1
+        local delta = {neighbour[1]-x, neighbour[2]-y}
+        local k = tostring(delta[1])..','..tostring(delta[2])
+        neebs[k] = true
       else
         region.perim = region.perim + 1
+      end
+    end
+
+     local dirs = {
+       left = neebs["-1,0"],
+       right = neebs["1,0"],
+       up = neebs["0,-1"],
+       down = neebs["0,1"],
+     }
+    --
+    -- local count = (left and 1 or 0)
+    --   + (right and 1 or 0)
+    --   + (up and 1 or 0)
+    --   + (down and 1 or 0)
+
+    if count == 0 then
+      region.sides = 4
+      print("lone", region.plant)
+    elseif count == 1 then
+      region.sides = region.sides + 4
+      print("end", region.plant)
+    elseif count == 2 then
+      if (dirs.up and dirs.down) or (dirs.left and dirs.right) then
+      else
+        print("bend", region.plant, region.sides)
+        region.sides = region.sides + 2
       end
     end
   end
 end
 
+print("SUM")
 price = 0
 for k,v in ipairs(allregions) do
-  price = price + v.area * v.perim
-  print(k, v.perim, v.area, v.plant)
+  price = price + v.area * v.sides
+  --print("price", v.plant, v.area * v.sides)
+  print("sides", v.plant, v.sides)
 end
 
 print(price)
